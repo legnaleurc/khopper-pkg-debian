e3f4b6169714d45e10424d8ebab6683fbd5305b2
legnaleurc <legnaleurc@gmail.com>
2009-11-02 12:25:20 +0000

    Change install helper script.

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #! /bin/sh
 
-PREFIX="/usr"
-MAIN="$1/$2${PREFIX}"
-CORE="$1/$3${PREFIX}"
-PLUGINS="$1/$4${PREFIX}"
+PREFIX="usr"
+MAIN="$1/khopper/${PREFIX}"
+CORE="$1/libkhopper-core/${PREFIX}"
+PLUGINS="$1/khopper-plugins/${PREFIX}"
 
 install -m 755 ./build/release/khopper ${MAIN}/bin
 install -m 644 ./main/image/logo.svg ${MAIN}/share/pixmaps/khopper.svg
 install -m 644 ./khopper.desktop ${MAIN}/share/applications
 
-install -m 644 ./build/release/libk_core.so.0.2.1 ${CORE}/lib
+install -m 644 ./build/release/libk_core.so.0.2.2 ${CORE}/lib
 
 install -m 644 ./build/release/plugins/*.so ${PLUGINS}/lib/khopper/plugins
