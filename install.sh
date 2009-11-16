56cab015d4d1ff50fda8b5f62ed37b61985eabe6
legnaleurc <legnaleurc@gmail.com>
2009-11-16 10:09:11 +0000

    Update for upstream.

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ install -m 755 ./build/release/khopper ${MAIN}/bin
 install -m 644 ./main/image/logo.svg ${MAIN}/share/pixmaps/khopper.svg
 install -m 644 ./khopper.desktop ${MAIN}/share/applications
 
-install -m 644 ./build/release/libk_core.so.0.2.2 ${CORE}/lib
+install -m 644 ./build/release/libk_core.so.0.2.3 ${CORE}/lib
+install -m 644 ./build/release/libkl_flac.so.0.2.3 ${CORE}/lib
 
 install -m 644 ./build/release/plugins/*.so ${PLUGINS}/lib/khopper/plugins
